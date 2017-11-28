3a6d1ed2c14e078bf803e1be4b0efed64bb18c7d
Robin <33178913+robin-libert@users.noreply.github.com>
2017-11-28 10:13:29 +0100

    create 2 process exemple

---
+++ create_process.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+int main(){
+	int pid_fils_gauche;
+	int pid_fils_droit;
+	printf("Je suis le pere %d\n", getpid());
+	pid_fils_gauche = fork();
+	if(pid_fils_gauche == 0){
+		printf("Je suis le fils gauche %d %d\n", getpid(), getppid());
+	}else{
+		wait(NULL);	
+		pid_fils_droit = fork();
+		if(pid_fils_droit == 0){
+			printf("Je suis le fils droit %d %d\n", getpid(), getppid());
+		}else{
+			wait(NULL);
+		}
+		
+	}
+	return 0;
+}
