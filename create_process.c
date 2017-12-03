7449442b00c90caa1b3b8dde554f30566ea521d4
Robin <33178913+robin-libert@users.noreply.github.com>
2017-12-03 13:50:02 +0100

    correction d'un oubli

--- create_process.c
+++ create_process.c
@@ -11,7 +11,7 @@ int main(){
 	if(pid_fils_gauche == 0){
 		printf("Je suis le fils gauche %d %d\n", getpid(), getppid());
 		exit(0);
-	}else{	
+	}
 	pid_fils_droit = fork();
 	if(pid_fils_droit == 0){
 		printf("Je suis le fils droit %d %d\n", getpid(), getppid());
