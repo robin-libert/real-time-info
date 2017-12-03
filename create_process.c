aa2a0be9e1ebbe5500c2e4318cafc7a101052bbe
Robin <33178913+robin-libert@users.noreply.github.com>
2017-12-03 13:49:01 +0100

    simplification du code

--- create_process.c
+++ create_process.c
@@ -10,15 +10,14 @@ int main(){
 	pid_fils_gauche = fork();
 	if(pid_fils_gauche == 0){
 		printf("Je suis le fils gauche %d %d\n", getpid(), getppid());
-	}else{
-		wait(NULL);	
-		pid_fils_droit = fork();
-		if(pid_fils_droit == 0){
-			printf("Je suis le fils droit %d %d\n", getpid(), getppid());
-		}else{
-			wait(NULL);
-		}
-		
+		exit(0);
+	}else{	
+	pid_fils_droit = fork();
+	if(pid_fils_droit == 0){
+		printf("Je suis le fils droit %d %d\n", getpid(), getppid());
+		exit(0);
 	}
+	wait(NULL);
+	wait(NULL);
 	return 0;
 }
